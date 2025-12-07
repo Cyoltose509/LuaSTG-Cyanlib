d0b7e3f329bba6ad2b6b5911d1c0ae0dc233ccf4
cyoltose <bilibilicookie@outlook.com>
2025-12-07 21:59:35 +0800

    Add bright_fog.png to Core asset library

--- game/Core/Assets/init.lua
+++ game/Core/Assets/init.lua
@@ -2,9 +2,10 @@ local Resource = Core.Resource
 Resource.SetResourcePool(Resource.PoolType.Global)
 
 local spath = "Core\\Assets\\Sprites\\"
-Resource.Image.NewFromFile("white", spath .. "white.png")
+Resource.Image.NewFromFile("white", spath .. "white.png",true)
 Resource.Image.NewFromFile("img_void", spath .. "img_void.png")
 Resource.Image.NewFromFile("bright", spath .. "bright.png",true)
+Resource.Image.NewFromFile("bright_fog", spath .. "bright_fog.png",true)
 Resource.Image.NewFromFile("bright_big", spath .. "bright_big.png",true)
 Resource.Image.NewFromFile("pure_circle", spath .. "pure_circle.png",true)
 local tex = Resource.Texture.New("particles", spath .. "particles.png")
