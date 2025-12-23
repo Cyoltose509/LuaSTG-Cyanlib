b22d7c87bded146c1a3284abf86850679f5ab1c9
cyoltose <bilibilicookie@outlook.com>
2025-12-23 13:15:51 +0800

    Enhance Core.Lib.StateMachine module

--- game/Core/Scripts/Lib/StateMachine.lua
+++ game/Core/Scripts/Lib/StateMachine.lua
@@ -6,15 +6,18 @@ Core.Lib.StateMachine = M
 local type = type
 local setmetatable = setmetatable
 
----@class Core.StateMachine.StateCallbacks
----@field onEnter function|nil @进入状态时的回调函数
----@field onExit function|nil @离开状态时的回调函数
----@field onUpdate function|nil @状态更新时的回调函数
+---@class Core.Lib.StateMachine.StateCallbacks
+---@field onEnter fun(context:Core.Lib.StateMachine.Context)|nil @进入状态时的回调函数
+---@field onExit fun(context:Core.Lib.StateMachine.Context)|nil @离开状态时的回调函数
+---@field onUpdate fun(context:Core.Lib.StateMachine.Context)|nil @状态更新时的回调函数
+
+
 
 
 
 ---@private
 function M:initialize()
+    ---@type Core.Lib.StateMachine.State[]
     self.states = {}
     self.stateCount = 0
     self.nameToId = {}
@@ -25,18 +28,29 @@ function M:initialize()
 
     self.currentStateId = 0
     self.previousStateId = 0
+    self.onStateChanged = nil
 
+    ---@class Core.Lib.StateMachine.Context
     self.context = {}
+end
+---@param name string @状态名称
+function M:hasState(name)
+    for _, state in pairs(self.states) do
+        if state.name == name then
+            return state
+        end
+    end
 end
 
 ---注册一个新状态
 ---@param name string @状态名称
----@param callbacks Core.StateMachine.StateCallbacks|nil @状态回调函数
+---@param callbacks Core.Lib.StateMachine.StateCallbacks|nil @状态回调函数
 function M:registerState(name, callbacks)
     local id = self.stateCount + 1
     self.stateCount = id
     self.nameToId[name] = id
 
+    ---@class Core.Lib.StateMachine.State
     local state = {
         id = id,
         name = name,
@@ -52,7 +66,7 @@ end
 ---添加状态转换
 ---@param fromState string|number @起始状态名称或 ID
 ---@param toState string|number @目标状态名称或 ID
----@param condition function|nil @转换条件函数，返回 true 时转换
+---@param condition fun(context:Core.Lib.StateMachine.Context)|nil @转换条件函数，返回 true 时转换
 function M:addTransition(fromState, toState, condition)
     local fromId = type(fromState) == "string" and self.nameToId[fromState] or fromState
     local toId = type(toState) == "string" and self.nameToId[toState] or toState
@@ -98,8 +112,16 @@ function M:setState(state)
     if nextState.onEnter then
         nextState.onEnter(self.context)
     end
+    if self.onStateChanged then
+        self:onStateChanged(currentState and currentState.name, nextState.name)
+    end
 
     return self
+end
+
+---@param func fun(from:string, to:string) @状态切换时的回调函数
+function M:setOnStateChanged(func)
+    self.onStateChanged = func
 end
 
 ---更新状态机
@@ -172,7 +194,7 @@ end
 
 ---创建一个新的状态机实例
 function M.New()
-    ---@type Core.StateMachine
+    ---@type Core.Lib.StateMachine
     local instance = {}
     setmetatable(instance, { __index = M })
     instance:initialize()
