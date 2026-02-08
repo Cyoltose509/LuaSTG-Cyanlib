f332d0b3e100e7a2e9dcf643399ff6b5e9bf90a1
cyoltose <bilibilicookie@outlook.com>
2026-02-08 20:16:40 +0800

    refactor(core): remove GameObject.AfterFrame event

--- game/Core/Scripts/MainLoop/Initialization.lua
+++ game/Core/Scripts/MainLoop/Initialization.lua
@@ -111,11 +111,6 @@ MainLoop.AddEvent("Frame", "Gameplay", {
     end,
     labels = { Label.Gameplay },
 })
-MainLoop.AddEvent("Frame", "Gameplay", {
-    name = "Core.GameObject.After",
-    func = Core.Object.GameObject.AfterFrame,
-    labels = { Label.Gameplay },
-})
 ------------------------------------
 MainLoop.AddEvent("Frame", "After", {
     name = "Core.Update.UI",
