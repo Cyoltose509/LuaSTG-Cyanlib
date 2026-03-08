4dfb3ae52067a6f53381a0ee0cccfaac14f1d7d8
cyoltose <bilibilicookie@outlook.com>
2026-03-08 23:01:08 +0800

    feat(i18n): add multi-language key name translations

---
+++ game/Core/Lang/init.lua
@@ -0,0 +1,10 @@
+local I18n = Core.I18n
+
+I18n.RegisterDirectory("Core", "Core/Lang/")
+I18n.RegisterLanguage("EN")
+I18n.RegisterLanguage("JA")
+I18n.RegisterLanguage("KO")
+I18n.RegisterLanguage("ZH-CN")
+I18n.RegisterLanguage("ZH-TW")
+
+
