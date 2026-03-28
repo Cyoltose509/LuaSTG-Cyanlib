da66c3d4da12137b9a47c40800101102dae31146
cyoltose <bilibilicookie@outlook.com>
2026-03-28 22:00:59 +0800

    refactor(render): update CJK text break rules

--- game/Core/Scripts/Render/Text/CJKBreak.lua
+++ game/Core/Scripts/Render/Text/CJKBreak.lua
@@ -18,15 +18,15 @@ do
             char_class[i] = type
         end
     end
-    add_range(0x4E00, 0x9FFF, M.Type.Normal)     -- CJK 统一表意文字
-    add_range(0x3400, 0x4DBF, M.Type.Normal)     -- Ext A
-    add_range(0x20000, 0x323AF, M.Type.Normal)   -- Ext B～I（常用区）
-    add_range(0xAC00, 0xD7A3, M.Type.Normal)     -- 韩文音节
-    add_range(0x3040, 0x30FF, M.Type.Normal)     -- 日文假名
-    add_range(0x31F0, 0x31FF, M.Type.Normal)     -- 假名扩展
-    add_range(0xFF01, 0xFF60, M.Type.Normal)     -- 全角 ASCII（大部分归0，个别后面覆盖）
-    char_class[0x3000] = M.Type.Normal        -- 全角空格
-    char_class[0x20] = M.Type.Normal        -- 半角空格
+    --add_range(0x4E00, 0x9FFF, M.Type.Normal)     -- CJK 统一表意文字
+    --add_range(0x3400, 0x4DBF, M.Type.Normal)     -- Ext A
+    --add_range(0x20000, 0x323AF, M.Type.Normal)   -- Ext B～I（常用区）
+    --add_range(0xAC00, 0xD7A3, M.Type.Normal)     -- 韩文音节
+    --add_range(0x3040, 0x30FF, M.Type.Normal)     -- 日文假名
+    --add_range(0x31F0, 0x31FF, M.Type.Normal)     -- 假名扩展
+    --add_range(0xFF01, 0xFF60, M.Type.Normal)     -- 全角 ASCII（大部分归0，个别后面覆盖）
+    --char_class[0x3000] = M.Type.Normal        -- 全角空格
+    --char_class[0x20] = M.Type.Normal        -- 半角空格
 
     -- 字母 & 数字（单词内部禁止断行）
     add_range(0x0030, 0x0039, M.Type.Letter)  -- 0-9
@@ -37,6 +37,9 @@ do
     add_range(0x0400, 0x052F, M.Type.Letter)  -- 西里尔
     add_range(0x2160, 0x217F, M.Type.Letter)  -- 罗马数字
 
+    for _, c in utf8.codes("⁰¹²³⁴⁵⁶⁷⁸⁹⁻") do
+        char_class[c] = M.Type.Letter
+    end
     -- 禁止行尾的开符号（Opening punctuation）
     for _, c in utf8.codes("(\"'（［【「『《〈＜〔｛〘〖〚｟［｢«‹〱〳〴") do
         char_class[c] = M.Type.Opening
